7d189f044e4aebff01ae97df37ad32abe148307e
Travis Martin <tjmartin2003@gmail.com>
2021-07-17 06:02:42 -0500

    Project setup.

---
+++ Source/Patches/CharacterCardPatch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WellMet.Patches {
+	[HarmonyPatch(typeof(CharacterCardUtility), "DrawCharacterCard")]
+	public class CharacterCardPatch {
+		[HarmonyTranspiler]
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
+			// TUTORIAL: https://harmony.pardeike.net/articles/patching-transpiler.html
+			return instructions; // TODO
+		}
+	}
+}
---
+++ Source/Patches/HarmonyPatcher.cs
@@ -0,0 +1,9 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace WellMet.Patches {
+	[StaticConstructorOnStartup]
+	public class HarmonyPatcher {
+		static HarmonyPatcher() => new Harmony("Lakuna.WellMet").PatchAll();
+	}
+}
