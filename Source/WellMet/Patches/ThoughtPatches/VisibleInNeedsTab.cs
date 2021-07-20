25706ad98f6adea9dc497702d56046d99a2db337
Travis Martin <tjmartin2003@gmail.com>
2021-07-20 09:56:09 -0500

    Completely hide thoughts from unknown traits in needs tab.

---
+++ Source/WellMet/Patches/ThoughtPatches/VisibleInNeedsTab.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Linq;
+
+namespace WellMet.Patches.ThoughtPatches {
+	[HarmonyPatch(typeof(Thought), "get_" + nameof(Thought.VisibleInNeedsTab))]
+	public class VisibleInNeedsTab {
+		[HarmonyPostfix]
+		public static void Postfix(Thought __instance, ref bool __result) {
+			if (__instance.def.requiredTraits != null && __instance.def.requiredTraits.All((traitDef) => __instance.pawn.story.traits.HasTrait(traitDef))) {
+				__result = false;
+			}
+
+			foreach (PawnRelationDef relationDef in __instance.pawn.GetRelations(__instance.pawn /* OTHER PAWN HERE */)) {
+
+			}
+		}
+	}
+}
