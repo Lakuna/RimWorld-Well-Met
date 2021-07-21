5661a88656b9fdfbba76ddb72e860af5576a5f86
Travis Martin <tjmartin2003@gmail.com>
2021-07-21 00:19:18 -0500

    OpinionExplanation patch placeholder.

---
+++ Source/WellMet/Patches/PawnRelationTrackerPatches/OpinionExplanation.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+
+namespace WellMet.Patches.PawnRelationTrackerPatches {
+	[HarmonyPatch(typeof(Pawn_RelationsTracker), nameof(Pawn_RelationsTracker.OpinionExplanation))]
+	public class OpinionExplanation {
+		[HarmonyTranspiler]
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
+			foreach (CodeInstruction instruction in instructions) {
+				yield return instruction;
+			}
+		}
+	}
+}
