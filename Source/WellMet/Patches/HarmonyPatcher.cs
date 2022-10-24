73c4bb0dadf7243016e6615fa3fbad30a68ec6e5
Travis Martin <tjmartin2003@gmail.com>
2022-10-24 18:20:51 -0500

    Multi-version support scaffolding.

--- Source/WellMet/Patches/HarmonyPatcher.cs
+++
@@ -1,9 +0,0 @@
-﻿using HarmonyLib;
-using Verse;
-
-namespace WellMet.Patches {
-	[StaticConstructorOnStartup]
-	public class HarmonyPatcher {
-		static HarmonyPatcher() => new Harmony("Lakuna.WellMet").PatchAll();
-	}
-}
