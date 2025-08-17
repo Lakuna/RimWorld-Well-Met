a649e70980e531f458db920efa9c550898ed94d5
Travis Martin <tjmartin2003@gmail.com>
2025-08-16 19:43:12 -0500

    Properly match `DrawSkill` patch.

--- Source/WellMet/WellMet/Patches/SkillUiPatches/DrawSkillPatch.cs
+++ Source/WellMet/WellMet/Patches/SkillUiPatches/DrawSkillPatch.cs
@@ -5,9 +5,11 @@ using HarmonyLib;
 #endif
 using Lakuna.WellMet.Utility;
 using RimWorld;
+using System;
+using UnityEngine;
 
 namespace Lakuna.WellMet.Patches.SkillUiPatches {
-	[HarmonyPatch(typeof(SkillUI), nameof(SkillUI.DrawSkill))]
+	[HarmonyPatch(typeof(SkillUI), nameof(SkillUI.DrawSkill), new Type[] { typeof(SkillRecord), typeof(Rect), typeof(SkillUI.SkillDrawMode), typeof(string) })]
 	internal static class DrawSkillPatch {
 		[HarmonyPrefix]
 		private static bool Prefix(SkillRecord skill) => KnowledgeUtility.IsSkillKnown(skill);
